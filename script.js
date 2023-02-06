8de2294df81299edd707925c79ace0445304bb6e
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-02-06 12:10:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,22 @@ function convertToRoman(num) {
     };
 
   //your code here
+let result = "";
+for (key in obj) {
+    result += key.repeat(Math.floor(num / obj[key]));
+    num %= obj[key];
+  }
+  
 
+
+	return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// You can test your code by running 
+//the above function and printing it to console 
+//by pressing the run button at the top. To run it 
+//with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+ console.log(convertToRoman(36));
 
 
 
